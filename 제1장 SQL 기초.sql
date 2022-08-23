68b0496d4e29a688224c9ba434a2031c49a5197e
nachttraum48 <nachttraum48@gmail.com>
2022-08-23 19:21:50 +0900

    2022/08/23 SQL 기초 수업예제

--- 제1장 SQL 기초.sql
+++ 제1장 SQL 기초.sql
@@ -49,10 +49,56 @@ delete from `User1` where `uid`='A102' and `age`=23;
 delete from `User1` where `age`>=30;
 
 #실습1-7
+create table `TblUser` (
+	`userId`   varchar(10),
+	`userName` varchar(10),
+	`userHp`   char(13),
+	`userAge`  tinyint,
+	`userAddr` varchar(20)    
+);
 
+create table `TblProduct` (
+	`prdCode`     int,
+	`prdName`     varchar(10),
+	`prdPrice`    int,
+	`prdAmount`   int,
+	`prdCompany`  varchar(10),
+	`prdMakeDate` date
+);
 
 #실습1-8
+insert into `TblUser` values ('p101', '김유신', '010-1234-1001', 25, '서울시 중구');
+insert into `TblUser` values ('p102', '김춘추', '010-1234-1002', 23, '부산시 금정구');
+insert into `TblUser` (`userId`, `userName`, `userAge`, `userAddr`) values ('p103', '장보고', 45, '경기도 광주군');
+insert into `TblUser` (`userId`, `userName`, `userAddr`) values ('p104', '강감찬', '경남 창원시');
+insert into `TblUser` (`userId`, `userName`, `userHp`, `userAge`) values ('p105', '이순신', '010-1234-1005', 50);
 
+insert into `TblProduct` values (1, '냉장고', 800, 10, 'LG', '2022-01-06');
+insert into `TblProduct` values (2, '노트북', 1200, 20, '삼성', '2022-01-06');
+insert into `TblProduct` values (3, 'TV', 1400, 6, 'LG', '2022-01-06');
+insert into `TblProduct` values (4, '세탁기', 1000, 8, 'LG', '2022-01-06');
+insert into `TblProduct` (`prdCode`, `prdName`, `prdPrice`, `prdAmount`) values (5, '컴퓨터', 1100, 0);
+insert into `TblProduct` values (6, '휴대폰', 900, 102, '삼성', '2022-01-06');
 
 #실습1-9
-
+select * from `TblUser`;
+select `userName` from `TblUser`;
+select `userName`, `userHp` from `TblUser`;
+select * from `TblUser` where `userId`='p102';
+select * from `TblUser` where `userId`='p104' or `userId`='p105';
+select * from `TblUser` where `userAddr`='신라';
+select * from `TblUser` where `userAge` > 30;
+select * from `TblUser` where `userHP` is null;
+update `TblUser` set `userAge`=42 where `userID`='p104';
+update `TblUser` set `userAddr`='경남 김해시' where `userId`='p105';
+delete from `TblUser` where `userId`='p103';
+select * from `TblProduct`;
+select `prdName` from `TblProduct`;
+select `prdName`, `prdPrice` from `TblProduct`;
+select * from `TblProduct` where `prdCompany`='LG';
+select * from `TblProduct` where `prdCompany`='삼성';
+update `TblProduct` set 
+					 `prdCompany`='삼성', 
+					 `prdMakeDate`='2021-01-01' 
+					where 
+					 `prdCode`=5
